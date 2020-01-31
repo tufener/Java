55719c7f1413ab35e5118ab6a6aeb7427e6405cc
Tufener <cadir.hasret@free.fr>
2020-01-31 16:05:13 +0100

    New project WorkspaceWeb

---
+++ CGIworkspaceJs/tpJS1/WebContent/page1.js
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+
+
+//function getInputValue(){
+//    // Selecting the input element and get its value 
+//    var inputVal = document.getElementById("myInput").value;
+//    
+//    document.getElementById("demo").innerHTML = "Bonjour " + inputVal
+//    // Displaying the value
+//    alert("Bonjour " + inputVal);
+//}
+
+function myFunction() { 
+    var x =  
+        document.getElementById("myText").value; 
+    
+    document.getElementById("demo").innerHTML = "Bonjour " + x; 
+} 
